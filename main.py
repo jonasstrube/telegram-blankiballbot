3fc29b7d263c4fd4633263c27c90f4f6067aa60b
jonasstrube <26897240+jonasstrube@users.noreply.github.com>
2021-06-02 19:53:10 +0200

    implement first question in spiel eintragen dialog

--- main.py
+++ main.py
@@ -3,11 +3,17 @@
 # pylint: disable=W0613, C0116, C0103
 # type: ignore[union-attr]
 
+# dependencies
+# pip install python-telegram-bot --upgrade
+# pip install requests
+
 import logging
 import os
 from datetime import (
     datetime
-) 
+)
+import requests
+import json
 
 from telegram import (
     Update,
@@ -39,7 +45,7 @@ logger = logging.getLogger(__name__)
 
 # --------------- global vars ----------------
 
-HOME_CHOOSING = range(1)
+HOME_CHOOSING, SPIEL_EINTRAGEN_TEAMAUSWAEHLEN = range(2)
 
 keyboard_main = [
     ['Spiel eintragen', 'Spielplan anzeigen'],
@@ -58,7 +64,22 @@ def start(update: Update, context: CallbackContext) -> int:
     return HOME_CHOOSING
 
 def spiel_eintragen(update: Update, context: CallbackContext) -> int:
-    update.message.reply_text('Danke fürs Eintragen!')
+    team_id = context.user_data.get('team_id')
+
+    # answer_api = requests.get('https://blankiball.de/api/team/read_opponents.php?id=' + team_id) # get all possible opponents of the team of the user
+    answer_api = requests.get('https://blankiball.de/api/team/read.php')
+    possible_opponent_teams: List = json.loads(answer_api.text)
+
+    # TODO iterate through the "possible_opponent_teams", get all names of them and distribute them through the keyboard (1x2, 1x3, 2x2, 3+2, 2x3)
+    keyboard_answer = [
+        ['dummy team 1 (dt1)', 'dummy team 2 (dt2)']    
+    ]
+
+    update.message.reply_text('Gegen welches Team habt ihr gespielt?', reply_markup=ReplyKeyboardMarkup(keyboard_answer))
+    return SPIEL_EINTRAGEN_TEAMAUSWAEHLEN
+
+def spiel_eintragen_ergebnisteam1(update: Update, context: CallbackContext) -> int:
+    update.message.reply_text('-- Dialog beendet --', reply_markup=ReplyKeyboardMarkup(keyboard_main))
     return HOME_CHOOSING
 
 def abbrechen(update: Update, context: CallbackContext) -> int:
@@ -80,7 +101,9 @@ def main():
         entry_points=[CommandHandler('start', start)],
         states={
             HOME_CHOOSING: [
-                MessageHandler(Filters.regex('^(' +  keyboard_main[0][0] +')$'), spiel_eintragen)]
+                MessageHandler(Filters.regex('^(' +  keyboard_main[0][0] +')$'), spiel_eintragen)],
+            SPIEL_EINTRAGEN_TEAMAUSWAEHLEN: [
+                MessageHandler(Filters.text, spiel_eintragen_ergebnisteam1)]
         },
         fallbacks=[MessageHandler(Filters.regex('^Abbrechen$'), abbrechen)],
         name="home_conversation",
