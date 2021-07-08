ad62d607b09cc508316f4f6f75370aaf496978e2
jonasstrube <26897240+jonasstrube@users.noreply.github.com>
2021-07-08 15:11:30 +0200

    change bot to teaser mode with only buttons for "time until tournament"

--- main.py
+++ main.py
@@ -10,7 +10,8 @@
 import logging
 import os
 from datetime import (
-    datetime
+    datetime,
+    timedelta
 )
 import requests
 import json
@@ -63,6 +64,13 @@ keyboard_main = [
     [keyboard_main_faq, keyboard_main_about, keyboard_main_settings]
 ]
 
+keyboard_main_teaser_how_long = 'Wann geht das Turnier endlich los? 😍'
+keyboard_main_teaser_HOW_LONG = 'WIE LANGE NOCH? 😡'
+keyboard_main_teaser = [
+    [keyboard_main_teaser_how_long], 
+    [keyboard_main_teaser_HOW_LONG]
+]
+
 keyboard_einstellungen_team_einstellen = "Team einstellen"
 keyboard_einstellungen = [
     [keyboard_einstellungen_team_einstellen]
@@ -76,46 +84,50 @@ def admin_status(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("last restart: " + date_string)
 
 def start(update: Update, context: CallbackContext) -> int: # after state HOME
-    update.message.reply_text("Hi! Schön dass du da bist", reply_markup = ReplyKeyboardMarkup(keyboard_main))
+    update.message.reply_text("Hi! Schön dass du da bist", reply_markup = ReplyKeyboardMarkup(keyboard_main_teaser))
     return HOME
 
 def spiel_eintragen(update: Update, context: CallbackContext) -> int:
-    team_id = context.user_data.get('team_id')
 
-    if team_id:
-        answer_api = requests.get('https://blankiball.de/api/team/read_opponents.php?id=' + str(team_id)) # get all possible opponents of the team of the user
-        possible_opponent_teams = json.loads(answer_api.text)['records']
+    update.message.reply_text('Das kann ich grad noch nicht', reply_markup=ReplyKeyboardMarkup(keyboard_main_teaser))
+    return HOME
 
-        # iterate through all possible opponent teams and distribute them through the keyboard (1: [1], 2: [1, 2], 3: [1, 2][3], 4: [1, 2][3, 4], 5: [1, 2, 3][4, 5], 6: [1, 2, 3][4, 5, 6], etc)
-        keyboard_answer = []
-        teams_count = len(possible_opponent_teams)
-        teams_index = 0
-        row_count = round(math.sqrt(teams_count))
-        average_column_count = teams_count / row_count
-        average_column_count_modulo = average_column_count % 1
-        for current_row in range(row_count):
-            if average_column_count_modulo == 0 or (current_row + 1) / row_count <= average_column_count_modulo + 0.0001:
-                column_count: int = math.ceil(average_column_count)
-            else:
-                column_count: int = math.ceil(average_column_count - 1)
-            single_row_content = []
-            for current_column in range(column_count):
-                current_team = possible_opponent_teams[teams_index]
-                teams_index += 1
-                single_row_content.append(current_team['name'] + " (" + current_team['kuerzel'] + ")")
-            keyboard_answer.append(single_row_content)
+    # team_id = context.user_data.get('team_id')
 
-        # TODO ihr seid grad in der Gruppenphase, gegen welches dieser Teams habt ihr gespielt?
-        update.message.reply_text('Gegen welches Team habt ihr gespielt?', reply_markup=ReplyKeyboardMarkup(keyboard_answer))
-        return SPIEL_EINTRAGEN__GEGNERAUSWAEHLEN
-    else:
-        # TODO update.message.reply_text('Ich weiß noch nicht in welchem Team du spielst, aber deiner Telefonnummer nach könntest du "Max" aus Team "Beispielteam" sein. Stimmt das?', reply_markup=ReplyKeyboardMarkup(keyboard_answer))
-        update.message.reply_text('Ich weiß noch nicht in welchem Team du spielst! Geh mal in die Einstellugen, da kannst du mir das schreiben', reply_markup=ReplyKeyboardMarkup(keyboard_main))
-        return HOME
+    # if team_id:
+    #     answer_api = requests.get('https://blankiball.de/api/team/read_opponents.php?id=' + str(team_id)) # get all possible opponents of the team of the user
+    #     possible_opponent_teams = json.loads(answer_api.text)['records']
+
+    #     # iterate through all possible opponent teams and distribute them through the keyboard (1: [1], 2: [1, 2], 3: [1, 2][3], 4: [1, 2][3, 4], 5: [1, 2, 3][4, 5], 6: [1, 2, 3][4, 5, 6], etc)
+    #     keyboard_answer = []
+    #     teams_count = len(possible_opponent_teams)
+    #     teams_index = 0
+    #     row_count = round(math.sqrt(teams_count))
+    #     average_column_count = teams_count / row_count
+    #     average_column_count_modulo = average_column_count % 1
+    #     for current_row in range(row_count):
+    #         if average_column_count_modulo == 0 or (current_row + 1) / row_count <= average_column_count_modulo + 0.0001:
+    #             column_count: int = math.ceil(average_column_count)
+    #         else:
+    #             column_count: int = math.ceil(average_column_count - 1)
+    #         single_row_content = []
+    #         for current_column in range(column_count):
+    #             current_team = possible_opponent_teams[teams_index]
+    #             teams_index += 1
+    #             single_row_content.append(current_team['name'] + " (" + current_team['kuerzel'] + ")")
+    #         keyboard_answer.append(single_row_content)
+
+    #     # TODO ihr seid grad in der Gruppenphase, gegen welches dieser Teams habt ihr gespielt?
+    #     update.message.reply_text('Gegen welches Team habt ihr gespielt?', reply_markup=ReplyKeyboardMarkup(keyboard_answer))
+    #     return SPIEL_EINTRAGEN__GEGNERAUSWAEHLEN
+    # else:
+    #     # TODO update.message.reply_text('Ich weiß noch nicht in welchem Team du spielst, aber deiner Telefonnummer nach könntest du "Max" aus Team "Beispielteam" sein. Stimmt das?', reply_markup=ReplyKeyboardMarkup(keyboard_answer))
+    #     update.message.reply_text('Ich weiß noch nicht in welchem Team du spielst! Geh mal in die Einstellugen, da kannst du mir das schreiben', reply_markup=ReplyKeyboardMarkup(keyboard_main))
+    #     return HOME
 
 def spiel_eintragen__ergebnis_erfragen_team1(update: Update, context: CallbackContext) -> int: # after state SPIEL_EINTRAGEN__GEGNERAUSWAEHLEN
     # TODO get bottles that the first team managed to drink
-    update.message.reply_text('-- Dialog beendet --', reply_markup=ReplyKeyboardMarkup(keyboard_main))
+    update.message.reply_text('-- Dialog beendet --', reply_markup=ReplyKeyboardMarkup(keyboard_main_teaser))
     return HOME
 
 def einstellungen_zeigen(update: Update, context: CallbackContext) -> int: # after state HOME_WAEHLEN
@@ -184,18 +196,49 @@ def einstellungen__team_aendern__team_verifizieren_und_speichern(update: Update,
         context.user_data['team_id'] = chosen_team['id']
         update.message.reply_text('Passwort stimmt ✅')
         update.message.reply_text('Nice! Du gehörst also zum Team "' + chosen_team['name'] + '" 👌')
-        update.message.reply_text('Jetzt kann ich auch deine Spielergebnisse eintragen oder dir deinen Spielplan zeigen', reply_markup=ReplyKeyboardMarkup(keyboard_main))
+        update.message.reply_text('Jetzt kann ich auch deine Spielergebnisse eintragen oder dir deinen Spielplan zeigen', reply_markup=ReplyKeyboardMarkup(keyboard_main_teaser))
     else:
         update.message.reply_text('Das Passwort ist nicht richtig 🙁')
         update.message.reply_text('Hast du dich vertippt? Oder hat dein Teamkapitän dich hops genommen?')
-        update.message.reply_sticker(sticker="CAACAgIAAxUAAWDHVbqxrxn5P7Y7oUyyaLMoJhK8AALGAAMfAUwVj1Fqci01g7gfBA", reply_markup=ReplyKeyboardMarkup(keyboard_main)) # sad macron sticker
+        update.message.reply_sticker(sticker="CAACAgIAAxUAAWDHVbqxrxn5P7Y7oUyyaLMoJhK8AALGAAMfAUwVj1Fqci01g7gfBA", reply_markup=ReplyKeyboardMarkup(keyboard_main_teaser)) # sad macron sticker
         
     del(context.chat_data['temp_einstellungen_team_aendern_chosen_team'])
 
     return HOME
 
+def zeit_normal(update: Update, context: CallbackContext) -> int: # after state HOME
+    
+    now = datetime.now()
+    then = datetime(2021, 9, 6)
+    time_until_tournament = datetime(2021, 9, 6) - datetime.now()
+    hours, remainder = divmod(time_until_tournament.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    days_string = 'Tage' if time_until_tournament.days != 1 else 'Tag'
+    hours_string = 'Stunden' if hours != 1 else 'Stunde'  
+    minutes_string = 'Minuten' if minutes != 1 else 'Minute'  
+    seconds_string = 'Sekunden' if seconds != 1 else 'Sekunde'
+    answer_string = 'Noch ' + str(time_until_tournament.days) + ' ' + days_string + ', ' + str(hours) + ' ' + hours_string + ', ' + str(minutes) + ' ' + minutes_string + ' und ' + str(seconds) + ' ' + seconds_string + ' durchhalten! Du schaffst das!'
+    update.message.reply_text(answer_string, reply_markup=ReplyKeyboardMarkup(keyboard_main_teaser))
+    return HOME
+
+def zeit_angeschrien(update: Update, context: CallbackContext) -> int: # after state HOME
+    now = datetime.now()
+    then = datetime(2021, 9, 6)
+    time_until_tournament = datetime(2021, 9, 6) - datetime.now()
+    hours, remainder = divmod(time_until_tournament.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    days_string = 'Tage' if time_until_tournament.days != 1 else 'Tag'
+    hours_string = 'Stunden' if hours != 1 else 'Stunde'  
+    minutes_string = 'Minuten' if minutes != 1 else 'Minute'  
+    seconds_string = 'Sekunden' if seconds != 1 else 'Sekunde'
+    answer_string = 'Noch ' + str(time_until_tournament.days) + ' ' + days_string + ', ' + str(hours) + ' ' + hours_string + ', ' + str(minutes) + ' ' + minutes_string + ' und ' + str(seconds) + ' ' + seconds_string + ' bis ich deine FRESSE POLIERE'
+    
+    update.message.reply_text('SCHREI MICH NICHT AN!')
+    update.message.reply_text(answer_string, reply_markup=ReplyKeyboardMarkup(keyboard_main_teaser))
+    return HOME
+
 def abbrechen(update: Update, context: CallbackContext) -> int:
-    update.message.reply_text("Jo, nix passiert", reply_markup = ReplyKeyboardMarkup(keyboard_main))
+    update.message.reply_text("Jo, nix passiert", reply_markup = ReplyKeyboardMarkup(keyboard_main_teaser))
     return HOME
 
 # ------------------ run -----------------------
@@ -214,7 +257,9 @@ def main():
         states={
             HOME: [
                 MessageHandler(Filters.regex('^(' +  keyboard_main_spiel_eintragen +')$'), spiel_eintragen),
-                MessageHandler(Filters.regex('^(' +  keyboard_main_settings +')$'), einstellungen_zeigen)
+                MessageHandler(Filters.regex('^(' +  keyboard_main_settings +')$'), einstellungen_zeigen),
+                MessageHandler(Filters.regex('^(' +  keyboard_main_teaser_how_long.replace('?', '\?') +')$'), zeit_normal),
+                MessageHandler(Filters.regex('^(' +  keyboard_main_teaser_HOW_LONG.replace('?', '\?') +')$'), zeit_angeschrien)
                 ],
             SPIEL_EINTRAGEN__GEGNERAUSWAEHLEN: [
                 MessageHandler(Filters.text, spiel_eintragen__ergebnis_erfragen_team1)],
